5474be36fb775908f413e2a06682097819634385
Saim <s.sad569.ss@gmail.com>
2024-02-05 18:18:14 +0500

    button size

--- components/ButtonGroup.js
+++ components/ButtonGroup.js
@@ -3,13 +3,13 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 const ButtonGroup = () => {
-  const [yesSize, setYesSize] = useState(16);
-  const [noSize, setNoSize] = useState(16);
+  const [yesSize, setYesSize] = useState(24);
+  const [noSize, setNoSize] = useState(24);
   const [currentNoIndex, setCurrentNoIndex] = useState(0);
   const [currentNoValue, setCurrentNoValue] = useState("No");
   const router = useRouter();
 
-  const noButton = ["Are you sure?", "Really sure?", "think again!", "Last chance!", "Are you really really sure?", "Give it another chance"];
+  const noButton = ["Are you sure?", "Really sure?", "think again!", "Last chance!", "Are you really really sure?", "This could be a mistake!", "Don't be so cold!", "Change of heart?", "Is this your final answer?", "Give it another chance", "You are breaking my heart!"];
 
   const onYes = () => {
     router.push('/aww')
@@ -17,7 +17,7 @@ const ButtonGroup = () => {
   };
 
   const onNo = () => {
-    setNoSize((prevSize) => prevSize / 1.2);
+    setNoSize((prevSize) => prevSize / 1.16);
     setYesSize((prevSize) => prevSize * 1.2);
 
     // Update the "No" button text to the next value in the array
@@ -29,14 +29,14 @@ const ButtonGroup = () => {
     <div className="justify-center space-x-5 text-center lg:max-w-5xl lg:w-full">
       <button
         style={{ fontSize: `${yesSize}px` }}
-        className="mb-3 font-semibold bg-green-400 dark:bg-green-600"
+        className="mb-3 font-semibold bg-green-400 dark:bg-green-600 hover:bg-green-300 dark:hover:bg-green-500"
         onClick={onYes}
       >
         Yes
       </button>
       <button
         style={{ fontSize: `${noSize}px` }}
-        className="mb-3 font-semibold bg-red-400 dark:bg-red-600"
+        className="mb-3 font-semibold bg-red-400 dark:bg-red-600 hover:bg-red-300 dark:hover:bg-red-500"
         onClick={onNo}
       >
         {currentNoValue}
